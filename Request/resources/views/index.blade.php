00e2b8431e08e32a3733328273f34d9846fc1df3
salmi2 <amine.salmi.solicode@gmail.com>
2022-10-21 10:16:45 +0100

    request

---
+++ Request/resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amine Salmi</title>
+</head>
+<body>
+    
+    <form action="">
+        @csrf
+
+        <input type="email" placeholder="email" name="input">
+        <input type="password" placeholder="password" name="input">
+        <button name="submit" >Submit</button>
+    </form>
+    
+    
+</body>
+</html>
