8ad2b8583702e1bfaebce37138e210de97df3e8f
salmi2 <amine.salmi.solicode@gmail.com>
2022-10-28 16:52:36 +0100

    .final v

--- realisation/resources/views/index.blade.php
+++ realisation/resources/views/index.blade.php
@@ -6,11 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
     <title>Document</title>
 </head>
 <body>
-    <button><a href="{{ route('Promotion.create') }}" class="btn btn-primary">add Promotion</a></button>
-    <input type="search" name="search" id="search" placeholder="search">
+    <button class="btn btn-primary" class="btn_add_promotion"><a href="{{ route('Promotion.create') }}" >add Promotion</a></button>
+    <input type="search" name="search" id="search" placeholder="search" class="searsh">
     <div id="content" >
 
         <table class="table">
